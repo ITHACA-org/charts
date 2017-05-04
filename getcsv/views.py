e7d62387c89b41453e6775c0c36ec94b93256e6d
sbsimo <simone.blb@gmail.com>
2017-05-04 12:54:52 +0200

    change table to adapt to real data

--- getcsv/views.py
+++ getcsv/views.py
@@ -10,7 +10,22 @@ def get_csv(request):
     response['Content-Disposition'] = 'attachment; filename="table.csv"'
 
     writer = csv.writer(response)
-    writer.writerow(['label', '1', '2', '3'])
-    writer.writerow(['label 2', '4', '5', '6'])
+    writer.writerow(['name', 'area'])
+    writer.writerow(['Af', 3.51])
+    writer.writerow(['Ems / ems', 0.00])
+    writer.writerow(['Ep / ep', 0.00])
+    writer.writerow(['Lmst', 4.88])
+    writer.writerow(['Mpb', 18.93])
+    writer.writerow(['Pf', 30.53])
+    writer.writerow(['Ppf', 6.84])
+    writer.writerow(['Qa', 0.49])
+    writer.writerow(['Qhac', 5.91])
+    writer.writerow(['Qhad', 0.00])
+    writer.writerow(['Qham', 11.01])
+    writer.writerow(['Qht1', 1.10])
+    writer.writerow(['Qht2', 27.60])
+    writer.writerow(['Qpf', 34.40])
+    writer.writerow(['Qphf', 42.30])
+
 
     return response
