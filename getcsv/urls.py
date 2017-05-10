3116b3d9528d78fe1105d2baec87577212d766ca
sbsimo <simone.blb@gmail.com>
2017-05-10 16:01:04 +0200

    add demo csv for aggregating data

--- getcsv/urls.py
+++ getcsv/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [url(r'^sample_csv$', views.get_csv, name='get_csv'),
+               url(r'^sample_csv2$', views.get_csv_aggr, name='get_csv2'),
                url(r'^pie_chart_v1$', views.pie_chart_v1),
                url(r'^donut_chart_v1$', views.donut_chart_v1),
                url(r'^pie_chart_v2$', views.pie_chart_v2),
--- getcsv/views.py
+++ getcsv/views.py
@@ -31,6 +31,32 @@ def get_csv(request):
 
     return response
 
+def get_csv_aggr(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="table.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['name', 'area'])
+    writer.writerow(['Af', 3.51])
+    writer.writerow(['Ems / ems', 0.00])
+    writer.writerow(['Ep / ep', 0.00])
+    writer.writerow(['Lmst', 4.88])
+    writer.writerow(['Mpb', 18.93])
+    writer.writerow(['Pf', 30.53])
+    writer.writerow(['Ppf', 6.84])
+    writer.writerow(['Qa', 0.49])
+    writer.writerow(['Qhac', 5.91])
+    writer.writerow(['Qhad', 0.00])
+    writer.writerow(['Qham', 11.01])
+    writer.writerow(['Qht1', 1.10])
+    writer.writerow(['Qht2', 27.60])
+    writer.writerow(['Qphf', 42.30])
+    writer.writerow(['Af', 6.58])
+    writer.writerow(['Qa', 30.78])
+    writer.writerow(['Af', 16.58])
+
+    return response
+
 
 def pie_chart_v1(request):
     return render(request, 'pie_chart_v1.html')
