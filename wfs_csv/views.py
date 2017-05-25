e53f3bf119a06d720679affa2a8370ade048c0f5
sbsimo <simone.blb@gmail.com>
2017-05-25 16:54:18 +0200

    manage parameters in csv request; add sample get request

--- wfs_csv/views.py
+++ wfs_csv/views.py
@@ -3,12 +3,17 @@ import urllib
 from django.http import HttpResponse
 
 def get_wfs_csv(request):
+    qdict = request.GET
+    typename = qdict['lyrname']
+    category_field = qdict['category']
+    quantity_field = qdict['quantity']
+
     params = urllib.urlencode({
         'service':'WFS',
         'version':'1.1.0',
         'request':'GetFeature',
-        'typename':'geonode:hti_geology_geology_pap_coxetalii_2011_polygon',
-        'propertyName':'name,area',
+        'typename':typename,
+        'propertyName':category_field + ',' + quantity_field,
         'outputFormat':'csv',
         'maxFeatures':'10'
     })
@@ -18,3 +23,4 @@ def get_wfs_csv(request):
     response = HttpResponse(content, content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="myfile.csv"'
     return response
+
