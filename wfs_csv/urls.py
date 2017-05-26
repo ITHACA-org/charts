f1cf83c423c6157dc4083387df27900c21b3feb7
sbsimo <simone.blb@gmail.com>
2017-05-26 17:22:14 +0200

    add a view that lists the available fields from a layer

--- wfs_csv/urls.py
+++ wfs_csv/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [url(r'^$', views.get_wfs_csv)]
+urlpatterns = [
+    url(r'^$', views.get_wfs_csv),
+    url(r'^fields$', views.get_fields)
+    ]
--- wfs_csv/views.py
+++ wfs_csv/views.py
@@ -1,6 +1,45 @@
 import urllib
+import xml.etree.ElementTree as ET
 
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def get_fields(request):
+    lyrname = request.GET['lyrname']
+    params = urllib.urlencode({
+        'service':'WFS',
+        'version':'1.1.0',
+        'request':'DescribeFeatureType',
+        'typename':lyrname
+    })
+
+    wfs_request = urllib.urlopen('http://localhost/geoserver/ows?%s'
+                                 % params)
+    content = wfs_request.read()
+
+    root = ET.fromstring(content)
+    namespace = root.tag.split('}')[0] + '}'
+    xpath = ('./{0}complexType/{0}complexContent/'
+             '{0}extension/{0}sequence/{0}element').format(namespace)
+    numeric_types = ['byte', 'decimal', 'int', 'integer', 'long', 'short',
+                     'boolean', 'double', 'float']
+    fields = {}
+    for element in root.findall(xpath):
+        type = element.attrib['type']
+        if not type.startswith('gml:'):
+            fields[element.attrib['name']] = type[4:]
+
+    fieldnames = fields.keys()
+    num_fieldnames = [k for k, v in fields.iteritems() if v in numeric_types]
+
+    context = {
+        'lyrname': lyrname,
+        'fieldnames': fieldnames,
+        'num_fieldnames': num_fieldnames
+    }
+    return render(request, 'wfs_csv/field_choice.html', context)
+
 
 def get_wfs_csv(request):
     qdict = request.GET
@@ -23,4 +62,3 @@ def get_wfs_csv(request):
     response = HttpResponse(content, content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="myfile.csv"'
     return response
-
