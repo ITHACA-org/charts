05814f3638afffbecc47282a124b0c958703269a
sbsimo <simone.blb@gmail.com>
2017-05-29 11:23:56 +0200

    change project into a geonode-project, version 2.6

--- charts/urls.py
+++ charts/urls.py
@@ -1,23 +1,13 @@
-"""charts URL Configuration
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-from django.conf.urls import include, url
-from django.contrib import admin
+from geonode.urls import *
 
-urlpatterns = [
+urlpatterns = patterns('',
+   url(r'^/?$',
+       TemplateView.as_view(template_name='site_index.html'),
+       name='home'),
+ ) + urlpatterns + [
     url(r'^charts/', include('getcsv.urls')),
     url(r'^table/', include('wfs_csv.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-]
+    ]
