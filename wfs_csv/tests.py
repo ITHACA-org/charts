5dd8ef166307b8efae7d4d25af50d25600e26fd7
sbsimo <simone.blb@gmail.com>
2017-05-30 14:44:49 +0200

    try to establish test env; not working properly, due to interference with catalogue service

--- wfs_csv/tests.py
+++ wfs_csv/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import urllib
+
+from django.test import SimpleTestCase
+from django.core.urlresolvers import reverse
+
+from wfs_csv import views
 
 # Create your tests here.
+class GetFieldsView(SimpleTestCase):
+    def test_non_existing_lyr(self):
+        # url = reverse(views.get_fields)
+        enc_lyrname = urllib.urlencode({'lyrname': 'idontexist'})
+        # complete_url = url + '?' + enc_lyrname
+        # self.client.get('/table')
+        # self.client.get(complete_url)
+        self.assertTrue(False)
+
+    def test_non_existing_field(self):
+        # self.client.get(reverse('wfs_csv:get_fields'))
+        self.assertTrue(False)
